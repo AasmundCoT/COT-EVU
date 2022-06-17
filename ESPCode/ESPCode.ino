156d2ac60e0c976fc99d9a1c71e3f6cdbeadccc3
TobiasCoT <tobias@cot.as>
2022-06-17 13:46:12 +0200

    Eksempel kode lagt til. Kode gjort mer "barnevennlig".

--- ESPCode/ESPCode.ino
+++ ESPCode/ESPCode.ino
@@ -2,13 +2,70 @@
 
 Car car("Nestingan_EXT", "Nestingan2019");
 
+double ntc;
+double prox;
+double line;
+
 void setup() {
     car.initCar();
 }
 
 void loop() {
     car.carLoop();
-    sendData(1, car.readData(NTC));
-    sendData(2, car.readData(PROX));
-    sendData(3, car.readData(LINE));
+    
+    ntc = car.readData(NTC);
+    prox = car.readData(PROX);
+    line = car.readData(LINE);
+
+    sendData(1, ntc);
+    sendData(2, prox);
+    sendData(3, line);
+}
+
+void w(bool state){
+  if(state==DOWN){
+    drive(100,100,FORWARD,FORWARD);
+  }else if(state==UP){
+    drive(0,0,FORWARD,FORWARD);
+  }
+}
+
+void a(bool state){
+  if(state==DOWN){
+    drive(100,100,BACKWARD,FORWARD);
+  }else if(state==UP){
+    drive(0,0,FORWARD,FORWARD);
+  }
+}
+
+void s(bool state){
+  if(state==DOWN){
+    drive(100,100,BACKWARD,BACKWARD);
+  }else if(state==UP){
+    drive(0,0,FORWARD,FORWARD);
+  }
+}
+
+void d(bool state){
+  if(state==DOWN){
+    drive(100,100,FORWARD,BACKWARD);
+  }else if(state==UP){
+    drive(0,0,FORWARD,FORWARD);
+  }
+}
+
+void q(bool state){
+  if(state==DOWN){
+    drive(0,100,FORWARD,FORWARD);
+  }else if(state==UP){
+    drive(0,0,FORWARD,FORWARD);
+  }
+}
+
+void e(bool state){
+  if(state==DOWN){
+    drive(100,0,FORWARD,FORWARD);
+  }else if(state==UP){
+    drive(0,0,FORWARD,FORWARD);
+  }
 }
